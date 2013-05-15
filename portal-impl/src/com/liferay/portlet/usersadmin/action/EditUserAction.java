ddabe67c435ee62c5aa44bae9237ff49a016d1de
Carl Peterken <carboes@gmail.com>
2013-05-16 10:54:00 +1200

    Fixed user categories admin. Added upgrade shell script to move newly built files into Liferay instance

--- portal-impl/src/com/liferay/portlet/usersadmin/action/EditUserAction.java
+++ portal-impl/src/com/liferay/portlet/usersadmin/action/EditUserAction.java
@@ -646,20 +646,20 @@ public class EditUserAction extends PortletAction {
 		ServiceContext serviceContext = ServiceContextFactory.getInstance(
 			User.class.getName(), actionRequest);
 
-		if (actionRequest.getParameter("assetCategoryNames") == null ) {
+		/*if (actionRequest.getParameter("assetCategoryNames") == null ) {
 			long[] assetCategoryIds =
 				AssetCategoryLocalServiceUtil.getCategoryIds(
 					User.class.getName(), user.getUserId());
 
 			serviceContext.setAssetCategoryIds(assetCategoryIds);
-		}
+		}*/
 
-		if (actionRequest.getParameter("assetTagNames") == null ) {
+		/*if (actionRequest.getParameter("assetTagNames") == null ) {
 			String[] assetTagNames = AssetTagLocalServiceUtil.getTagNames(
 				User.class.getName(), user.getUserId());
 
 			serviceContext.setAssetTagNames(assetTagNames);
-		}
+		}*/
 
 		user = UserServiceUtil.updateUser(
 			user.getUserId(), oldPassword, newPassword1, newPassword2,
---
+++ upgrade.sh
@@ -0,0 +1,2 @@
+ant all
+cp -pRv ../bundles/resin-4.0.29 ../../workspace/liferay-portal-6.1.1-ce-ga2
